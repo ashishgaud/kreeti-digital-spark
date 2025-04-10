ab728d1677ae09b89fcd24f5f826781fa757f1c2
ashishgaud <142683282+ashishgaud@users.noreply.github.com>
2025-04-11 04:15:48 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = () => {
     {
       id: 1,
       title: '1BHK Launch Campaign',
-       thumbnailUrl: 'https://via.placeholder.com/640x360/0A2647/FFFFFF?text=Digital+Marketing+Portfolio+%7C+Click+to+Play'
+       thumbnailUrl: 'https://via.placeholder.com/640x360/0A2647/FFFFFF?text=Digital+Marketing+Portfolio+%7C+Click+to+Play',
 
       videoUrl: 'https://pqolkeztotszaehikkdq.supabase.co/storage/v1/object/public/kreeti//WhatsApp%20Video%202025-04-11%20at%201.35.09%20AM.mp4'
     },
